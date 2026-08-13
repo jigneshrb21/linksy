7dfe39993630bf4f43f20f3095b2cf51d7b735c8
Jignesh Awasare <jigneshrb21@gmail.com>
2026-08-14 01:17:03 +0530

    initial spring boot skeleton for linksy

---
+++ src/main/java/com/jignesh/linksy/LinksyApplication.java
@@ -0,0 +1,13 @@
+package com.jignesh.linksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jignesh/linksy/LinksyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jignesh.linksy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
